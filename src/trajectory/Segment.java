d04ea3d9e41d562e08afb33174fce43852252e1e
Vedaad Shakib <vedaad799@gmail.com>
2016-07-23 15:46:24 -0700

    Added array serialization and unit tests

--- src/trajectory/Segment.java
+++ src/trajectory/Segment.java
@@ -1,26 +1,35 @@
 package trajectory;
 
-public class Segment{
-
+public class Segment {
     double velocity;
     double acceleration;
     double position;
 
-    public Segment(double velocity, double acceleration, double position){
+    public Segment(double velocity, double acceleration, double position) {
         this.velocity = velocity;
         this.acceleration = acceleration;
         this.position = position;
     }
 
-    public double getVelocity(){
+    public Segment(String velocity, String acceleration, String position) {
+        this.velocity = Double.parseDouble(velocity);
+        this.acceleration = Double.parseDouble(acceleration);
+        this.position = Double.parseDouble(position);
+    }
+
+    public double getVelocity() {
         return this.velocity;
     }
 
-    public double getAcceleration(){
+    public double getAcceleration() {
         return this.acceleration;
     }
 
-    public double getPosition(){
+    public double getPosition() {
         return this.position;
+    }
+
+    public String toString() {
+	return velocity+" "+acceleration+" "+position;
     }
 } 
